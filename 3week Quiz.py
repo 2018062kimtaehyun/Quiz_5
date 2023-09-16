1f04068b7740482ae70694d40d899bcee0f0bcd2
2018062kimtaehyun <144421337+2018062kimtaehyun@users.noreply.github.com>
2023-09-17 00:21:59 +0900

    Create 3week Quiz.py
    컴퓨터 프로그래밍2 3주차 퀴즈입니다.

---
+++ 3week Quiz.py
@@ -0,0 +1,5 @@
+table = {"달러": 1320, "엔": 950, "위안": 182}
+money = [13, 200, 13]
+total = table["달러"] * money[0] + table["엔"] * money[1] + table["위안"] * money[2]
+a = total
+print("철수가 가지고 있는 돈의 원화 가치는", a,"원 입니다.")
