e45509fd2bc3dcce395f4e7a46d46d47d6a41146
2018062kimtaehyun <144421337+2018062kimtaehyun@users.noreply.github.com>
2023-10-06 16:06:34 +0900

    Create Quiz_4.py

---
+++ Quiz_4.py
@@ -0,0 +1,16 @@
+def print_multiplication_table(n):
+    for i in range(1, n + 1):
+        print(f"------[ {i} 단 ] ------")
+        for j in range(1, 10):
+            result = i * j
+            print(f"{i} X {j} = {result}")
+
+try:
+    num = int(input("몇 단까지 출력할까요? "))
+
+    if num < 1 or num > 19:
+        print("1부터 19까지의 숫자를 입력하세요.")
+    else:
+        print_multiplication_table(num)
+except ValueError:
+    print("올바른 숫자를 입력하세요.")
